a958256af5eef90ff258e1241f7b129439591ee4
Bastien Boymond <71839723+BastienBoymond@users.noreply.github.com>
2021-10-24 20:50:07 +0200

    first api version in flask does

---
+++ api.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+from VostfreeScrapperPy.page import getLastNewsPage, getAnimesByGenreAndPage
+from VostfreeScrapperPy.anime import getAnimeById, getAnimeByUrl
+from VostfreeScrapperPy.search import getSearchTerm
+
+app = Flask(__name__)
+
+@app.route('/anime/<int:id>', methods=['GET'])
+def AnimeById(id):
+    return jsonify(getAnimeById(id)) , 200
+
+@app.route('/anime/url', methods=['GET'])
+def AnimeByUrl():
+    url = request.args.get('url')
+    return jsonify(getAnimeByUrl(url)) , 200
+
+@app.route('/anime/search', methods=['GET'])
+def AnimeSearch():
+    term = request.args.get('term')
+    return jsonify(getSearchTerm(term)) , 200
+
+@app.route('/anime/lastnews', methods=['GET'])
+def AnimeLastNews():
+    return jsonify(getLastNewsPage(1)) , 200
+
+@app.route('/anime/news/<int:page>', methods=['GET'])
+def AnimeNews(page):
+    return jsonify(getLastNewsPage(page)) , 200
+
+@app.route('/anime/genre/<string:genre>/<int:page>', methods=['GET'])
+def AnimeGenre(genre, page):
+    return jsonify(getAnimesByGenreAndPage(genre, page)) , 200
+
+app.run(port=2900)
